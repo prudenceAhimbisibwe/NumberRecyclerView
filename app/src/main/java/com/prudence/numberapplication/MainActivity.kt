af9e4bdda757ab6ba876029f4d7d9df6987ee63c
PudenceAhimbisibwe <prudeahimbisibwe@gmail.com>
2022-07-02 08:05:07 +0300

    fibonacci changes

--- app/src/main/java/com/prudence/numberapplication/MainActivity.kt
+++ app/src/main/java/com/prudence/numberapplication/MainActivity.kt
@@ -12,12 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        displayNumbers()
+        fibonacci_100(100)
     }
-    fun displayNumbers(){
-        val numberList= listOf(0,1,1,2,3,5,8,13,21,34,55,99)
-        binding.rvNumber.layoutManager=LinearLayoutManager(this)
-        val numbersAdaptor=NumberRecyclerAdaptor(numberList )
-        binding.rvNumber.adapter=numbersAdaptor
+    fun fibonacci_100(size:Int):List<Int> {
+        var Number = ArrayList<Int>()
+        var start = 0
+        var step = 1
+        var sumNumber = 0
+        while (sumNumber <= size) {
+            print(start)
+            var sumNumber = start + step
+            start = step
+            sumNumber++
+            Number += sumNumber
+        }
+        return Number
     }
-}
+
+    }
